b383e41caeb59d20d54220050b5e7891cb1b2ee0
Chris Renfrow <dev@chrisrenfrow.me>
2023-11-17 20:25:14 -0800

    Add something resembling lighting
    Not sure what's going on tbh

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,31 @@ use std::f32::consts::PI;
 use std::fs::File;
 use std::io::{self, Write};
 
-use nalgebra::Vector3;
+use nalgebra::{Matrix3x1, SimdValue, Vector3};
+
+#[derive(Debug, Clone, Copy)]
+struct Light {
+    position: Vector3<f32>,
+    intensity: f32,
+}
+
+impl Light {
+    fn new(position: Vector3<f32>, intensity: f32) -> Self {
+        Self {
+            position,
+            intensity,
+        }
+    }
+
+    fn pos(&self) -> Vector3<f32> {
+        self.position.clone()
+    }
+
+    fn diffuse_for_intersection(&self, intersection: &Intersection) -> f32 {
+        let direction = (self.pos() - intersection.point).normalize();
+        self.intensity * f32::max(0.0, (direction * intersection.distance).norm())
+    }
+}
 
 #[derive(Debug, Clone, Copy)]
 struct Material {
@@ -38,7 +62,7 @@ impl Sphere {
         &self,
         origin: Vector3<f32>,
         direction: Vector3<f32>, /* _distance: f32 */
-    ) -> Option<Material> {
+    ) -> Option<Intersection> {
         // Calculate the direction vector of the line segment
         let dir_normalized = direction.normalize();
         // Calculate the vector from the line start to the sphere center
@@ -51,10 +75,51 @@ impl Sphere {
         let distance = (closest_point - self.center).norm();
         // Check if the closest point is within the sphere
         if distance <= self.radius {
-            Some(self.material)
+            Some(Intersection::new(closest_point, distance, self.material))
         } else {
             None
         }
+    }
+}
+
+#[derive(Debug)]
+struct Intersection {
+    point: Vector3<f32>,
+    distance: f32,
+    material: Material,
+}
+
+impl Intersection {
+    fn new(point: Vector3<f32>, distance: f32, material: Material) -> Self {
+        Self {
+            point,
+            distance,
+            material,
+        }
+    }
+}
+
+fn scene_intersect(
+    origin: Vector3<f32>,
+    direction: Vector3<f32>,
+    spheres: &Vec<Sphere>,
+) -> Option<Intersection> {
+    // Find the nearest intersection if it exists and return it
+    let nearest = spheres
+        .iter()
+        .fold(None, |nearest: Option<Intersection>, sphere| {
+            match sphere.ray_intersect(origin, direction) {
+                Some(intersection) => match nearest {
+                    Some(nearest) if intersection.distance < nearest.distance => Some(intersection),
+                    _ => Some(intersection),
+                },
+                None => nearest,
+            }
+        });
+
+    match nearest {
+        Some(intersection) if intersection.distance < 1000.0 => Some(intersection),
+        _ => None,
     }
 }
 
@@ -62,25 +127,18 @@ fn cast_ray(
     origin: Vector3<f32>,
     direction: Vector3<f32>,
     spheres: &Vec<Sphere>,
+    lights: &Vec<Light>,
 ) -> Option<Vector3<f32>> {
-    // Maybe get the closest sphere first? How do we do that?
-    //
-    // A sphere with a small radius could be occluded by a sphere that is farther away
-    // but has a very large radius.
-    //
-    // If we can get the distance between the origin and the center of the sphere plus
-    // the radius, then we can compare each sphere in this manner and determine the
-    // closest one, using that one to determine our ray intersection.
-    //
-    // But this won't work for other, more complicated shapes.
-    let sphere: &Sphere = &spheres[0];
-    match spheres
-        .iter()
-        .find(|s| s.ray_intersect(origin, direction).is_some())
-    {
-        Some(s) => Some(s.material.diffuse()),
-        None => None,
-    }
+    let intersection = match scene_intersect(origin, direction, spheres) {
+        Some(intersection) => intersection,
+        _ => return None,
+    };
+
+    let diffuse_intensity: f32 = lights.iter().fold(0.0, |acc: f32, light: &Light| {
+        acc + light.diffuse_for_intersection(&intersection)
+    });
+
+    Some(intersection.material.diffuse() as Vector3<f32> + Vector3::repeat(diffuse_intensity))
 }
 
 fn write_ppm_image(filename: &str, width: u32, height: u32, pixels: &[u8]) -> io::Result<()> {
@@ -105,9 +163,9 @@ fn vec_to_rgb(v: Vector3<f32>) -> (u8, u8, u8) {
     )
 }
 
-fn render(spheres: Vec<Sphere>) -> io::Result<()> {
-    let width = 256 * 2;
-    let height = 256 * 2;
+fn render(spheres: &Vec<Sphere>, lights: &Vec<Light>) -> io::Result<()> {
+    let width = 256;
+    let height = 256;
     let fov = PI / 3.0;
     let filename = "output.ppm";
 
@@ -119,7 +177,7 @@ fn render(spheres: Vec<Sphere>) -> io::Result<()> {
                 * (width as f32 / height as f32);
             let y = -(2.0 * (j as f32 + 0.5) / height as f32 - 1.0) * (fov / 2.0).tan();
             let dir = Vector3::new(x, y, -1.0).normalize();
-            let (r, g, b) = match cast_ray(Vector3::zeros(), dir, &spheres) {
+            let (r, g, b) = match cast_ray(Vector3::zeros(), dir, &spheres, &lights) {
                 Some(v) => vec_to_rgb(v),
                 _ => vec_to_rgb(Vector3::new(
                     j as f32 / height as f32,
@@ -140,9 +198,11 @@ fn main() -> io::Result<()> {
     let chartreuse = Material::new(Vector3::new(0.5, 0.8, 0.3));
     let red = Material::new(Vector3::new(1.0, 0.5, 0.5));
     let spheres = vec![
-        Sphere::new(Vector3::new(-3.0, 0.0, -16.0), 2.0, chartreuse),
         Sphere::new(Vector3::new(2.0, 1.0, -16.0), 5.0, red),
+        Sphere::new(Vector3::new(2.0, 3.0, -11.0), 1.0, chartreuse),
+        Sphere::new(Vector3::new(-3.0, 0.0, -16.0), 2.0, chartreuse),
     ];
-    render(spheres)?;
+    let lights = vec![Light::new(Vector3::new(-20.0, 20.0, 20.0), 0.2)];
+    render(&spheres, &lights)?;
     Ok(())
 }
